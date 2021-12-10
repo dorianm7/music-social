29da67a6d9a7b43db9e7553e7c2639585c77f361
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2021-12-10 12:37:20 -0800

    Allow list inner elements to fill width of component

--- src/components/subcomponents/ExpandedOptions.js
+++ src/components/subcomponents/ExpandedOptions.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
-import '../../stylesheets/ExpandedOptions.css';
+import '../../stylesheets/subcomponents/ExpandedOptions.css';
 
 function createListElements(options) {
   const listItems = [];
