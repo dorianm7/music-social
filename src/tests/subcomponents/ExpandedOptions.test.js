68511f63ffa88a711049a7da712f1b6c323725b0
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2021-12-10 10:34:03 -0800

    Test correct number of options

---
+++ src/tests/subcomponents/ExpandedOptions.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import ExpandedOptions from '../../components/subcomponents/ExpandedOptions';
+
+let container = null;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it('renders correct number of options', () => {
+  act(() => {
+    render(
+      <>
+        <ExpandedOptions />
+        <ExpandedOptions options={['One']} />
+        <ExpandedOptions options={[
+          'One',
+          <ul>
+            <li>
+              Two
+              <p>i</p>
+            </li>
+            <li>i</li>
+          </ul>]}
+        />
+      </>,
+      container,
+    );
+  });
+
+  const expandedOptionsAr = container.querySelectorAll('.expanded-options');
+
+  expect(expandedOptionsAr[0].children.length).toBe(3);
+  expect(expandedOptionsAr[1].children.length).toBe(1);
+  expect(expandedOptionsAr[2].children.length).toBe(2);
+});
+
+it('renders the correct curved corners', () => {
+
+});
