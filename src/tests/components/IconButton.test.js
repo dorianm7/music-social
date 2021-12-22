f3adbdf7cdbb01ab993ccebcb9e8ab9bdfa24e7e
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2021-12-21 18:43:17 -0800

    Add iconWidth,iconHeight props and test them

--- src/tests/components/IconButton.test.js
+++ src/tests/components/IconButton.test.js
@@ -32,3 +32,27 @@ it('renders correct alt-text', () => {
 
   expect(container.querySelector('img').alt).toBe(testText);
 });
+
+it('renders correct height and width', () => {
+  const defaultWidth = '20px';
+  const defaultHeight = '20px';
+  const testWidths = [defaultWidth, '30px', defaultWidth, '30px'];
+  const testHeights = [defaultHeight, defaultHeight, '30px', '30px'];
+  act(() => {
+    render(
+      <>
+        <IconButton />
+        <IconButton iconWidth={testWidths[1]} />
+        <IconButton iconHeight={testHeights[2]} />
+        <IconButton iconWidth={testWidths[3]} iconHeight={testHeights[3]} />
+      </>,
+      container,
+    );
+  });
+
+  const imgEls = container.querySelector('img');
+  for (let i = 0; i < imgEls.length; i += 1) {
+    expect(imgEls[i].width).toBe(testWidths[i]);
+    expect(imgEls[i].height).toBe(testHeights[i]);
+  }
+});
