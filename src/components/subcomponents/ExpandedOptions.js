8a96e5a2c8a25875c426bf4d3dc5590b872cb8b3
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2021-12-23 02:48:19 -0800

    Add title and align props

--- src/components/subcomponents/ExpandedOptions.js
+++ src/components/subcomponents/ExpandedOptions.js
@@ -2,13 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import '../../stylesheets/subcomponents/ExpandedOptions.css';
-import '../../stylesheets/main.css';
+// import '../../stylesheets/main.css';
 
-function createListElements(options) {
+function createListElements(title, alignTitle, options, alignOptions) {
   const listItems = [];
 
+  if (title !== 'Title') {
+    listItems.push(<li key={nanoid()} className={`options-title align-${alignTitle}`}>{title}</li>);
+  }
   for (let i = 0; i < options.length; i += 1) {
-    listItems.push(<li key={nanoid()}>{options[i]}</li>);
+    listItems.push(<li key={nanoid()} className={`options align-${alignOptions}`}>{options[i]}</li>);
   }
 
   return listItems;
@@ -16,11 +19,14 @@ function createListElements(options) {
 
 function ExpandedOptions(props) {
   const {
+    title,
+    alignTitle,
     options,
+    alignOptions,
     corner,
   } = props;
   const cornerClassName = `${corner}-corner`;
-  const listItems = createListElements(options);
+  const listItems = createListElements(title, alignTitle, options, alignOptions);
 
   return (
     <ul className={`${cornerClassName} expanded-options `}>
@@ -30,19 +36,31 @@ function ExpandedOptions(props) {
 }
 
 ExpandedOptions.propTypes = {
+  title: PropTypes.string,
+  alignTitle: PropTypes.string,
   options: PropTypes.arrayOf(PropTypes.node),
+  alignOptions: PropTypes.string,
   corner: PropTypes.string,
 };
 
 ExpandedOptions.defaultProps = {
+  title: 'Title',
+  alignTitle: 'center',
   options: ['Default', 'expanded', 'options'],
+  alignOptions: 'center',
   corner: 'top-left',
 };
 
 const RefExpandedOptions = React.forwardRef((props, ref) => {
-  const { options, corner } = props;
+  const {
+    title,
+    alignTitle,
+    options,
+    alignOptions,
+    corner,
+  } = props;
   const cornerClassName = `${corner}-corner`;
-  const listItems = createListElements(options);
+  const listItems = createListElements(title, alignTitle, options, alignOptions);
 
   return (
     <ul ref={ref} className={`${cornerClassName} expanded-options `}>
@@ -52,12 +70,18 @@ const RefExpandedOptions = React.forwardRef((props, ref) => {
 });
 
 RefExpandedOptions.propTypes = {
+  title: PropTypes.string,
+  alignTitle: PropTypes.string,
   options: PropTypes.arrayOf(PropTypes.node),
+  alignOptions: PropTypes.string,
   corner: PropTypes.string,
 };
 
 RefExpandedOptions.defaultProps = {
+  title: 'Title',
+  alignTitle: 'center',
   options: ['Default', 'expanded', 'ref', 'options'],
+  alignOptions: 'center',
   corner: 'top-left',
 };
 
