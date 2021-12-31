63da118e4b73f83d17f3cc574e56f2711f21a49f
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2021-12-30 19:41:34 -0800

    Implement initial modal

---
+++ src/components/modals/Modal.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../../stylesheets/Modal.css';
+
+function Modal(props) {
+  const {
+    heading,
+    contents,
+  } = props;
+  return (
+    <div className="modal rounded-all">
+      <h1 className="modal-heading">{heading}</h1>
+      <div className="modal-content">
+        {contents}
+      </div>
+    </div>
+  );
+}
+
+Modal.propTypes = {
+  heading: PropTypes.string,
+  contents: PropTypes.node,
+};
+
+Modal.defaultProps = {
+  heading: 'Modal',
+  contents: (
+    <>
+      Here is some text
+      <p>Here is a paragraph element</p>
+      Here is some text
+    </>
+  ),
+};
+
+export default Modal;
