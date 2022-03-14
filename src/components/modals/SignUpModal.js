c215e9e3d92a0ef3b5af4ff376603c3eee5aaebe
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2022-03-13 23:08:39 -0700

    Implement SignUpModal using modal components

--- src/components/modals/SignUpModal.js
+++ src/components/modals/SignUpModal.js
@@ -1,54 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Modal from './Modal';
-import SignUpForm from '../forms/SignUpForm';
-import '../../stylesheets/SignUpModal.css';
+import SignUpModalContents from './contents/SignUpModalContents';
 
 function SignUpModal(props) {
   const {
-    closeHandler,
-    onSubmitSuccess,
+    onClose,
+    formOnSubmit,
   } = props;
-
-  // TODO use signUp from auth functions
-  const handleSubmit = (email, password) => {
-    // signUp: imported from auth functions.
-    // successSubmit: used to move to next step in sign up process
-    // errorSubmit: used to show feedback error has happened
-    // signUp(email, password,
-    //  () => {
-    //  successSubmit();
-    //  onSubmitSuccess();},
-    //  errorSubmit)
-    console.log(`Submit clicked: ${email} ${password}`);
-    onSubmitSuccess();
-  };
-
-  const contents = (
-    <>
-      <SignUpForm onSubmit={handleSubmit} />
-      <hr />
-      <a className="move-to-log-in" href="#placeholder">Move to Log in</a>
-    </>
-  );
 
   return (
     <Modal
       heading="Sign Up"
-      contents={contents}
-      closeHandler={closeHandler}
+      contents={(
+        <SignUpModalContents
+          formOnSubmit={formOnSubmit}
+        />
+      )}
+      closeHandler={onClose}
     />
   );
 }
 
 SignUpModal.propTypes = {
-  closeHandler: PropTypes.func,
-  onSubmitSuccess: PropTypes.func,
+  onClose: PropTypes.func,
+  formOnSubmit: PropTypes.func,
 };
 
 SignUpModal.defaultProps = {
-  closeHandler: () => { window.alert('Close handled'); },
-  onSubmitSuccess: () => { window.alert('Submit success. Move to next step'); },
+  onClose: () => { window.alert('Close handled'); },
+  formOnSubmit: () => { window.alert('Modal form submitted'); },
 };
 
 export default SignUpModal;
