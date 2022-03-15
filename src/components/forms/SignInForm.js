e07833d9079f76b7d4823a7a72817b0a9776f783
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2022-03-14 18:43:22 -0700

    Implement SignInForm with mock SignIn function

---
+++ src/components/forms/SignInForm.js
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import ToggleIconButton from '../basic/ToggleIconButton';
+import {
+  CLOSED_EYE_NAME,
+  OPEN_EYE_NAME,
+} from '../../Icons';
+import '../../stylesheets/main.css';
+import '../../stylesheets/forms/SignInForm.css';
+
+function SignInForm(props) {
+  const {
+    onSubmit,
+    submitSuccess,
+  } = props;
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handlePasswordToggleIcon = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const mockSignIn = (
+      userEmail = '',
+      userPassword = '',
+      successCallback = () => {},
+      errorCallback = () => {},
+    ) => {
+      if (submitSuccess) {
+        console.log(`${userEmail} ${userPassword}`); // Just to use arguments
+        successCallback();
+      } else {
+        const submittingError = new Error('Email or password not valid');
+        errorCallback(submittingError);
+        setError(submittingError);
+      }
+    };
+    mockSignIn(
+      email,
+      password,
+      () => { onSubmit(); },
+      () => {},
+    );
+  };
+
+  const passwordInputType = showPassword ? 'text' : 'password';
+
+  return (
+    <div className="sign-in-form">
+      <form onSubmit={handleSubmit}>
+        <label>
+          <div className="label-bar">
+            <p className="label-text">Email</p>
+          </div>
+          <input
+            className="full-width-input"
+            type="text"
+            name="email"
+            required
+          />
+        </label>
+        <label>
+          <div className="label-bar">
+            <p className="label-text">Password</p>
+            <ToggleIconButton
+              toggle={showPassword}
+              initialIcon={CLOSED_EYE_NAME}
+              subsequentIcon={OPEN_EYE_NAME}
+              iconWidth="15px"
+              iconHeight="15px"
+              initialOnClick={handlePasswordToggleIcon}
+              subsequentOnClick={handlePasswordToggleIcon}
+              initialTransparent
+              subsequentTransparent
+            />
+          </div>
+          <input
+            className="full-width-input"
+            type={passwordInputType}
+            name="password"
+            required
+          />
+        </label>
+        <button
+          className="sign-in-button basic-button"
+          type="submit"
+        >
+          Sign In
+        </button>
+        {error && (
+          <span className="error-message">{error.message}</span>
+        )}
+      </form>
+    </div>
+  );
+}
+
+SignInForm.propTypes = {
+  onSubmit: PropTypes.func,
+  submitSuccess: PropTypes.bool,
+};
+
+SignInForm.defaultProps = {
+  onSubmit: () => { window.alert('Successfully submitted'); },
+  submitSuccess: false,
+};
+
+export default SignInForm;
