e3b58b0c10a193ad856b83b41871dd2ab6dd6d0d
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2022-05-17 12:10:04 -0700

    Add headingType and playlistName props

--- src/components/PlaylistHeader.js
+++ src/components/PlaylistHeader.js
@@ -3,6 +3,31 @@ import PropTypes from 'prop-types';
 
 import '../stylesheets/PlaylistHeader.css';
 
+function renderHeadingElement(headingType, playlistName) {
+  let res;
+  switch (headingType) {
+    case 'h1':
+      res = <h1>{playlistName}</h1>;
+      break;
+    case 'h2':
+      res = <h2>{playlistName}</h2>;
+      break;
+    case 'h3':
+      res = <h3>{playlistName}</h3>;
+      break;
+    case 'h4':
+      res = <h4>{playlistName}</h4>;
+      break;
+    case 'h5':
+      res = <h5>{playlistName}</h5>;
+      break;
+    default:
+      res = <h6>{playlistName}</h6>;
+      break;
+  }
+  return res;
+}
+
 function getReadableTime(timeMS) {
   const hrs = Math.floor(Number(timeMS) / (1000 * 60 * 60));
   const mins = Math.floor((timeMS - (hrs * 1000 * 60 * 60)) / (1000 * 60));
@@ -12,13 +37,15 @@ function getReadableTime(timeMS) {
 // Will render running time if totalRunningTime > 0
 function PlaylistHeader(props) {
   const {
+    headingType,
+    playlistName,
     totalSongs,
     totalRunningTime,
   } = props;
 
   return (
     <div className="playlist-header">
-      <h2>Playlist</h2>
+      {renderHeadingElement(headingType, playlistName)}
       <span>
         {totalSongs}
         &nbsp;songs
@@ -34,11 +61,15 @@ function PlaylistHeader(props) {
 }
 
 PlaylistHeader.propTypes = {
+  headingType: PropTypes.string,
+  playlistName: PropTypes.string,
   totalSongs: PropTypes.string,
   totalRunningTime: PropTypes.string,
 };
 
 PlaylistHeader.defaultProps = {
+  headingType: 'h2',
+  playlistName: 'Playlist',
   totalSongs: '50',
   totalRunningTime: '-1',
 };
