a9e772d83a39dfe7837dd0a484d2f1efc59b23c4
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2022-05-20 21:40:59 -0700

    Convert MusicLibrary to container component

--- src/components/MusicLibrary.js
+++ src/components/MusicLibrary.js
@@ -1,97 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import '../stylesheets/MusicLibrary.css';
 
 import BasicPlaylist from './BasicPlaylist';
-import PlaylistHeader from './PlaylistHeader';
-import playlist from '../local_data/Playlist_0.json';
 
-function renderPlaylistHeader(headingType, playlistName, totalSongs, totalRunningTime) {
-  return (
-    <PlaylistHeader
-      headingType={headingType}
-      playlistName={playlistName}
-      totalSongs={totalSongs}
-      totalRunningTime={totalRunningTime}
-    />
-  );
-}
-
-// playlistsItems length should match the number of playlists
 function MusicLibrary(props) {
   const {
-    playlistsHeadingType,
-    playlists,
+    children,
   } = props;
-
-  const [searchText, setSearchText] = useState('');
 
   return (
     <div className="music-library">
-      <BasicPlaylist
-        playlistHeader={(
-          renderPlaylistHeader(
-            playlistsHeadingType,
-            'Artists',
-            playlists[0].total,
-            -1, // Default Value
-          )
-        )}
-        items={playlists[0].items}
-        onSearchStringChange={setSearchText}
-        showSearch
-      />
-      <BasicPlaylist
-        playlistHeader={(
-          renderPlaylistHeader(
-            playlistsHeadingType,
-            'Albums',
-            playlists[1].total,
-            -1, // Default Value
-          )
-        )}
-        items={playlists[1].items}
-        searchVal={searchText}
-      />
-      <BasicPlaylist
-        playlistHeader={(
-          renderPlaylistHeader(
-            playlistsHeadingType,
-            'Songs',
-            playlists[2].total,
-            -1, // Default Value
-          )
-        )}
-        items={playlists[2].items}
-        searchVal={searchText}
-      />
+      {children}
     </div>
   );
 }
 
 MusicLibrary.propTypes = {
-  playlistsHeadingType: PropTypes.string,
-  playlists: PropTypes.arrayOf(PropTypes.shape({
-    __comment: PropTypes.string,
-    href: PropTypes.string,
-    items: PropTypes.arrayOf(PropTypes.object),
-    limit: PropTypes.number,
-    next: PropTypes.string,
-    offset: PropTypes.number,
-    previous: PropTypes.string,
-    total: PropTypes.number,
-  })),
+  children: PropTypes.node,
 };
 
 MusicLibrary.defaultProps = {
-  playlistsHeadingType: 'h2',
-  playlists:
-  [
-    playlist,
-    playlist,
-    playlist,
-  ],
+  children: <BasicPlaylist />,
 };
 
 export default MusicLibrary;
