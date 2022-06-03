ed533dcd3338c92507d70d0553f856851f5bc3d2
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2022-06-03 14:41:02 -0700

    Add signInOnClick prop

--- src/components/modals/contents/SignUpModalContents.js
+++ src/components/modals/contents/SignUpModalContents.js
@@ -6,6 +6,7 @@ import '../../../stylesheets/SignUpModalContents.css';
 function SignUpModalContents(props) {
   const {
     formOnSubmit,
+    signInOnClick,
   } = props;
 
   return (
@@ -15,17 +16,25 @@ function SignUpModalContents(props) {
         submitSuccess // TODO remove when SignUpForm backend connected
       />
       <hr />
-      <a className="move-to-log-in" href="#placeholder">Move to Log in</a>
+      <button
+        type="button"
+        className="anchor-like"
+        onClick={signInOnClick}
+      >
+        Move&nbsp;to&nbsp;Sign&nbsp;in
+      </button>
     </>
   );
 }
 
 SignUpModalContents.propTypes = {
   formOnSubmit: PropTypes.func,
+  signInOnClick: PropTypes.func,
 };
 
 SignUpModalContents.defaultProps = {
   formOnSubmit: () => { window.alert('Modal contents submit'); },
+  signInOnClick: () => { window.alert('Move to sign in clicked'); },
 };
 
 export default SignUpModalContents;
