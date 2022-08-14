185b4a74c5282926493a170b59524408260cb02b
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2022-08-14 00:21:11 -0700

    Implement Select story

---
+++ src/components/Select/Select.stories.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+
+import Select from './Select';
+
+export default {
+  title: 'Components/Select',
+  component: Select,
+};
+
+const Template = (args) => <Select {...args} />;
+
+export const Default = Template.bind({});
+Default.args = Select.defaultProps;
