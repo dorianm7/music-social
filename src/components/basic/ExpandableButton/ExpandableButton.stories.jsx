4639717ed1c9b8532d4c624c12389cd48a299f96
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2022-08-13 22:57:59 -0700

    Implement ExpandableButton story

---
+++ src/components/basic/ExpandableButton/ExpandableButton.stories.jsx
@@ -0,0 +1,72 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+
+import ToggleIconButton from '../ToggleIconButton/ToggleIconButton';
+import ExpandableButton from './ExpandableButton';
+
+import { IconNames } from '../../../Icons';
+
+export default {
+  title: 'Components/Basic/ExpandableButton',
+  component: ExpandableButton,
+  subcomponents: { ToggleIconButton },
+  argTypes: {
+    initialIcon: {
+      options: Object.values(IconNames),
+      control: { type: 'select' },
+    },
+    subsequentIcon: {
+      options: Object.values(IconNames),
+      control: { type: 'select' },
+    },
+    expand: {
+      options: [
+        'none',
+        'up',
+        'right',
+        'down',
+        'left',
+      ],
+      control: { type: 'select' },
+    },
+    direction: {
+      options: [
+        'none',
+        'up',
+        'down',
+        'left',
+        'right',
+      ],
+      control: { type: 'select' },
+    },
+    alignOptionsTitle: {
+      options: [
+        'left',
+        'center',
+        'right',
+      ],
+      control: { type: 'select' },
+    },
+    alignOptions: {
+      options: [
+        'left',
+        'center',
+        'right',
+      ],
+      control: { type: 'select' },
+    },
+  },
+};
+
+const Template = (args) => <ExpandableButton {...args} />;
+
+export const Default = Template.bind({});
+Default.args = ExpandableButton.defaultProps;
+Default.args.expand = 'right';
+Default.args.direction = 'down';
+
+export const OptionsTitle = Template.bind({});
+OptionsTitle.args = {
+  ...Default.args,
+  optionsTitle: 'Options Title',
+};
