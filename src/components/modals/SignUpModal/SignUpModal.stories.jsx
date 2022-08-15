2bf58d8c7844199ed0ab92d37deb9eac28878db7
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2022-08-14 20:21:01 -0700

    Implement SignUpModal story

---
+++ src/components/modals/SignUpModal/SignUpModal.stories.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+
+import SignUpModal from './SignUpModal';
+import SignUpModalContents from '../../modal-contents/SignUpModalContents/SignUpModalContents';
+
+export default {
+  title: 'Components/Modals/SignUpModal',
+  component: SignUpModal,
+  subcomponents: { SignUpModalContents },
+};
+
+const Template = (args) => <SignUpModal {...args} />;
+
+export const Default = Template.bind({});
+Default.args = SignUpModal.defaultProps;
