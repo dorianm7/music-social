2c01efb096589dfb934c86ec0e13c76607634325
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2022-08-14 18:53:11 -0700

    Implement SignInForm story

---
+++ src/components/forms/SignInForm/SignInForm.stories.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+
+import SignInForm from './SignInForm';
+
+export default {
+  title: 'Components/forms/SignInForm',
+  component: SignInForm,
+};
+
+const Template = (args) => <SignInForm {...args} />;
+
+export const Default = Template.bind({});
+Default.args = SignInForm.defaultProps;
