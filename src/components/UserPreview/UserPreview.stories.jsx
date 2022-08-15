f1d40b343907ae80394da1cbc0d1f797e993c7d4
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2022-08-14 23:30:31 -0700

    Add Following story

--- src/components/UserPreview/UserPreview.stories.jsx
+++ src/components/UserPreview/UserPreview.stories.jsx
@@ -12,3 +12,9 @@ const Template = (args) => <UserPreview {...args} />;
 
 export const Default = Template.bind({});
 Default.args = UserPreview.defaultProps;
+
+export const Following = Template.bind({});
+Following.args = {
+  ...Default.args,
+  isFollowing: true,
+};
