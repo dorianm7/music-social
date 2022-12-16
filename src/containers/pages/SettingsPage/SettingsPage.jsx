87d8f1ed85fc6e8809f227aaff5970b978a31cea
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2022-12-16 01:58:20 -0800

    Implement outline of account deletion with toast feedback. Wont work with current user.uid

--- src/containers/pages/SettingsPage/SettingsPage.jsx
+++ src/containers/pages/SettingsPage/SettingsPage.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import {
+  React,
+  useState,
+} from 'react';
 import PropTypes from 'prop-types';
 import InAppPage from '../../InAppPage/InAppPage';
 import SettingsPageContents from '../../page-contents/SettingsPageContents/SettingsPageContents';
 import Toast from '../../../components/Toast/Toast';
+import Modal from '../../../components/modals/Modal/Modal';
+import SignInModalContents from '../../../components/modal-contents/SignInModalContents/SignInModalContents';
 
-import { deleteUserAccount } from '../../../firebase/auth-firebase';
+import {
+  deleteUserAccount,
+  emailPasswordSignIn,
+  googleSignIn,
+} from '../../../firebase/auth-firebase';
+import { useUserContext } from '../../../contexts/UserContext';
+import deleteUser from '../../../backend/user/user';
 
 function SettingsPage(props) {
   const {
@@ -17,32 +28,79 @@ function SettingsPage(props) {
     toast,
     toastMessage,
   } = props;
+  const [modalOpen, setModalOpen] = useState(false);
 
-  const deleteAccountHandler = async () => {
-    await deleteUserAccount(
-      () => {
-        // Delete from Mongo
-      },
-      (err) => {
-        toast(err.message, 4000);
-      },
-    );
-  };
+  // TODO Figure out how/where to store correct uid
+  const user = useUserContext();
+
+  const deleteFromFirebase = () => deleteUserAccount()
+    .catch((err) => {
+      throw new Error(err.code);
+    });
+
+  const deleteFromMongo = () => deleteUser(user.uid)
+    .catch((err) => {
+      let errorMsg;
+      if (err.response) {
+        if (err.response.status === 400) {
+          errorMsg = err.response.data.detail;
+        } else {
+          errorMsg = 'Internal error. Try again';
+        }
+      } else if (err.request) {
+        errorMsg = 'Didn\'t receive response from server';
+      } else {
+        errorMsg = 'Something weird happened. Try again';
+      }
+      throw new Error(errorMsg);
+    });
+
+  const deleteAccounts = () => deleteFromMongo()
+    .then(() => deleteFromFirebase())
+    .catch((err) => toast(err.message, 4000));
+
+  // TODO Create/use re-sign in Modal
+  const signInModalContents = (
+    <SignInModalContents
+      formOnSubmit={async (email, password) => {
+        await emailPasswordSignIn(
+          email,
+          password,
+          deleteAccounts,
+          (err) => toast(err.message, 4000),
+        );
+      }}
+      googleSignInOnClick={() => {
+        googleSignIn(
+          deleteAccounts,
+          (err) => toast(err.message, 4000),
+        );
+      }}
+    />
+  );
 
   return (
-    <InAppPage
-      pageTitle="Settings"
-      sideMenuOnClick={sideMenuOnClick}
-      notificationsOnClick={notificationsOnClick}
-      hasNotification={hasNotification}
-    >
-      <SettingsPageContents
-        deleteAccountOnClick={deleteAccountHandler}
+    <>
+      <InAppPage
+        pageTitle="Settings"
+        sideMenuOnClick={sideMenuOnClick}
+        notificationsOnClick={notificationsOnClick}
+        hasNotification={hasNotification}
+      >
+        <SettingsPageContents
+          deleteAccountOnClick={() => setModalOpen(true)}
+        />
+      </InAppPage>
+      <Modal
+        heading="Sign In"
+        contents={signInModalContents}
+        closeHandler={() => setModalOpen(false)}
+        open={modalOpen}
       />
       {toastVisible && (
         <Toast message={toastMessage} />
       )}
-    </InAppPage>
+    </>
   );
 }
 
