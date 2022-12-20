cceccc559ec45a681ca78d4ab2e5f2521ce0ab39
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2022-12-20 01:41:24 -0800

    Stub SpotifyAuthorizeCallbackPage

---
+++ src/containers/pages/SpotifyAuthorizeCallbackPage/SpotifyAuthorizeCallbackPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+function SpotifyAuthorizeCallbackPage() {
+  // get access_token from querystring
+  // get fromUri from querystring
+  // set localstorage access_token
+  // navigate back to settings page
+  return <>Placeholder</>;
+}
+
+export default SpotifyAuthorizeCallbackPage;
