4ab620e8be317afa2f2f451bb7b62434303fa5c9
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2022-12-20 21:08:18 -0800

    Check for errors, store data in localstorage

--- src/containers/pages/SpotifyAuthorizeCallbackPage/SpotifyAuthorizeCallbackPage.jsx
+++ src/containers/pages/SpotifyAuthorizeCallbackPage/SpotifyAuthorizeCallbackPage.jsx
@@ -11,10 +11,27 @@ function SpotifyAuthorizeCallbackPage() {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
   useEffect(() => {
-    const accessToken = searchParams.get('access_token');
     const fromPath = searchParams.get('fromPath');
-    localStorage.setItem('spotify_access_token', accessToken);
-    navigate(fromPath);
+    const error = searchParams.get('error');
+    let redirectPath;
+    if (error) {
+      const detail = searchParams.get('detail');
+      const urlParamsObj = {
+        error,
+        detail,
+      };
+      const urlParams = new URLSearchParams(urlParamsObj);
+      // only adding errors to redirect page query string for now
+      redirectPath = `${fromPath}/?${urlParams.toString()}`;
+    } else {
+      const accessToken = searchParams.get('access_token');
+      const expires = searchParams.get('expires_in');
+      localStorage.setItem('spotify_access_token', accessToken);
+      localStorage.setItem('spotify_access_token_expires_in', expires);
+      localStorage.setItem('spotify_access_token_timestamp', Date.now());
+      redirectPath = fromPath;
+    }
+    navigate(redirectPath);
   }, []);
   return <></>;
 }
