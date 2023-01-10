ea12fc5672dd3ca97b663090a06886505542fd9f
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2023-01-10 15:04:19 -0800

    Create and use userEndpoint function

--- src/backend/user/user.js
+++ src/backend/user/user.js
@@ -4,8 +4,8 @@
  */
 
 import axios from 'axios';
-import { USERS_BACKEND_ENDPOINT } from '../users/users-helpers';
+import { userEndpoint } from '../users/users-helpers';
 
-const deleteUser = (userId) => axios.delete(`${USERS_BACKEND_ENDPOINT}/${userId}`);
+const deleteUser = (userId) => axios.delete(userEndpoint(userId));
 
 export default deleteUser;
--- src/backend/users/users-helpers.js
+++ src/backend/users/users-helpers.js
@@ -1,10 +1,18 @@
 /**
- * Module containing functions to help working with Users
+ * Module containing constants,functions to help working with Users
  * @module users-helpers
  */
 
 const USERS_BACKEND_ENDPOINT = `${process.env.REACT_APP_BACKEND_HTTP_SERVER}/users`;
 
+/**
+ * Returns the backend endpoint of user with given userId
+ * @param {string} userId Id of user
+ * @returns {string} Endpoint of user with given userId
+ */
+const userEndpoint = (userId) => `${USERS_BACKEND_ENDPOINT}/${userId}`;
+
 export {
   USERS_BACKEND_ENDPOINT,
+  userEndpoint,
 };
