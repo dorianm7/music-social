13610d8b3fd1cccd917c3f6ca75d27e421ba64b4
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2023-01-10 15:34:53 -0800

    Add patchUser function and documentation

--- src/backend/user/user.js
+++ src/backend/user/user.js
@@ -6,6 +6,30 @@
 import axios from 'axios';
 import { userEndpoint } from '../users/users-helpers';
 
+/**
+ * Deletes user from the database
+ * @param {string} userId Id of user
+ * @returns {Promise} promise of a successful delete
+ */
 const deleteUser = (userId) => axios.delete(userEndpoint(userId));
 
-export default deleteUser;
+/**
+ * Patch user in the database
+ * @param {string} userId Id of user
+ * @param {object[]} patchBody Array of patch object operations to be executed
+ * @returns {Promise} Promise of a successful patch
+ */
+const patchUser = (userId, patchBody) => axios.patch(
+  userEndpoint(userId),
+  patchBody,
+  {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  },
+);
+
+export {
+  deleteUser,
+  patchUser,
+};
