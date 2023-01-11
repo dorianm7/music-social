ec60cc72c866f4dfb6867a05382070b6ba0c5927
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2023-01-10 16:42:13 -0800

    Create functions to remove local and remote Spotify tokens

--- src/backend/spotify/spotify.js
+++ src/backend/spotify/spotify.js
@@ -4,6 +4,7 @@
  */
 
 import axios from 'axios';
+import { patchUser } from '../user/user';
 import {
   SPOTIFY_LOCAL_STORAGE_KEYS,
   SPOTIFY_LOCAL_STORAGE_VALUES,
@@ -42,6 +43,37 @@ const getAccessToken = async (uid) => {
 };
 
 /**
+ * Remove local Spotify access token values
+ */
+const removeAccessToken = () => {
+  const {
+    accessToken,
+    expiresIn,
+    timestamp,
+  } = SPOTIFY_LOCAL_STORAGE_KEYS;
+  localStorage.removeItem(accessToken);
+  localStorage.removeItem(expiresIn);
+  localStorage.removeItem(timestamp);
+};
+
+/**
+ * Remove local and remote Spotify tokens
+ * @param {string} uid Id of user to remove stored refresh token
+ * @returns {Promise} A promise of a successful patch
+ */
+const removeTokens = (uid) => {
+  removeAccessToken();
+  const patchBody = [
+    {
+      op: 'replace',
+      path: '/spotify_refresh_token',
+      value: '',
+    },
+  ];
+  return patchUser(uid, patchBody);
+};
+
+/**
  * Check if user has authorized Spotify
  * @returns {boolean} Whether user has authorized Spotify
  */
@@ -50,5 +82,6 @@ const isAuthorized = () => !!SPOTIFY_LOCAL_STORAGE_VALUES.accessToken;
 export {
   refreshTokens,
   getAccessToken,
+  removeTokens,
   isAuthorized,
 };
