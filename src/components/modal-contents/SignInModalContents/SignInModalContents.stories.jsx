7718a1f720f65307d6ce5526bfc33896cb243e26
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2023-01-11 12:55:35 -0800

    Add props used for Resigning in

--- src/components/modal-contents/SignInModalContents/SignInModalContents.stories.jsx
+++ src/components/modal-contents/SignInModalContents/SignInModalContents.stories.jsx
@@ -1,5 +1,9 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import {
+  EmailAuthProvider,
+  GoogleAuthProvider,
+} from 'firebase/auth';
 
 import SignInModalContents from './SignInModalContents';
 import SignInForm from '../../forms/SignInForm/SignInForm';
@@ -7,6 +11,15 @@ import SignInForm from '../../forms/SignInForm/SignInForm';
 export default {
   title: 'Components/ModalContents/SignInModalContents',
   component: SignInModalContents,
+  argTypes: {
+    providerId: {
+      options: [
+        EmailAuthProvider.PROVIDER_ID,
+        GoogleAuthProvider.PROVIDER_ID,
+      ],
+      control: { type: 'select' },
+    },
+  },
   subcomponents: { SignInForm },
 };
 
@@ -14,3 +27,17 @@ const Template = (args) => <SignInModalContents {...args} />;
 
 export const Default = Template.bind({});
 Default.args = SignInModalContents.defaultProps;
+
+export const EmailResignIn = Template.bind({});
+EmailResignIn.args = {
+  ...Default.args,
+  providerId: EmailAuthProvider.PROVIDER_ID,
+  resigningIn: true,
+};
+
+export const GoogleResignIn = Template.bind({});
+GoogleResignIn.args = {
+  ...Default.args,
+  providerId: GoogleAuthProvider.PROVIDER_ID,
+  resigningIn: true,
+};
