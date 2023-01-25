8165aaf9ec2a326c9b01885118f3dd9a8ba96b87
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2023-01-24 20:18:14 -0800

    Create helper functions for the user endpoint

--- src/backend/users/users-helpers.js
+++ src/backend/users/users-helpers.js
@@ -10,9 +10,30 @@ const USERS_BACKEND_ENDPOINT = `${process.env.REACT_APP_BACKEND_HTTP_SERVER}/use
  * @param {string} userId Id of user
  * @returns {string} Endpoint of user with given userId
  */
-const userEndpoint = (userId) => `${USERS_BACKEND_ENDPOINT}/${userId}`;
+const userBaseEndpoint = (userId) => `${USERS_BACKEND_ENDPOINT}/${userId}`;
+
+/**
+ * Returns the param string for fields given list of fields
+ * @param {string[]} fields List of fields
+ * @returns {string} Search param string for fields
+ */
+const fieldsSearchParam = (fields) => `fields=${fields.toString()}`;
+
+/**
+ * Return backend endpoint that will return fields of user with given userId
+ * @param {string} userId User id
+ * @param {string[]} fields List fields
+ * @returns {string} Endpoint of user with given userId containing properties in fields
+ */
+const userEndpoint = (userId, fields) => {
+  const userBase = userBaseEndpoint(userId);
+  return (fields.length > 0)
+    ? `${userBase}/?${fieldsSearchParam(fields)}`
+    : userBase;
+};
 
 export {
   USERS_BACKEND_ENDPOINT,
+  userBaseEndpoint,
   userEndpoint,
 };
