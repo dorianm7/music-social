83ad75d7621e83d1d2d27f23c14606d08bce0a55
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2023-01-30 13:41:28 -0800

    Add response status checks return data and document types

--- src/backend/spotify/spotify.js
+++ src/backend/spotify/spotify.js
@@ -6,11 +6,35 @@
 import axios from 'axios';
 
 /**
+ * Object representing a Spotify response of limited offset items
+ * @typedef {Object} SpotifyLimitOffsetResponse
+ * @property {string} href Link to the Web API endpoint returning the full result of the request
+ * @property {Object[]} items Requested content
+ * @property {int} limit Maximum number of items in the response
+ * @property {string | null} next Url to the next page of items
+ * @property {int} offset Offset of the items returned
+ * @property {string | null} previous Url to the previous page of items
+ * @property {int} total Total number of items available to return
+ */
+
+/**
+ * Object representing a Spotify response of limited cursored items
+ * @typedef {Object} SpotifyLimitCursorResponse
+ * @property {string} href Link to the Web API endpoint returning the full result of the request
+ * @property {Object[]} items Requested content
+ * @property {int} limit Maximum number of items in the response
+ * @property {string | null} next Url to the next page of items
+ * @property {Object} cursors Cursors used to find the next set of items
+ * @property {int} total Total number of items available to return
+ */
+
+/**
  * Get limit amount of Spotify albums starting from offset
  * @param {int} limit Amount of albums to return
  * @param {int} offset Index of first item to return
  * @param {string} accessToken Spotify access token
- * @returns {Promise} Promise of axios response object containing user spotify albums
+ * @returns {Promise<SpotifyLimitOffsetResponse>} Promise of response object containing Albums
+ * @see {@link https://developer.spotify.com/documentation/web-api/reference/#/operations/get-users-saved-albums}
  */
 const getAlbums = (limit = 50, offset = 0, accessToken) => axios.get(
   `https://api.spotify.com/v1/me/albums?limit=${limit}&offset=${offset}`,
@@ -19,7 +43,22 @@ const getAlbums = (limit = 50, offset = 0, accessToken) => axios.get(
       Authorization: `Bearer ${accessToken}`,
     },
   },
-);
+)
+  .then((res) => res.data)
+  .catch((err) => {
+    if (err.response) {
+      if (err.response.status > 499) {
+        return Promise.reject(new Error('Unexpected error. Try again.'));
+      }
+      if (err.response.status === 429) {
+        return Promise.reject(new Error('Error. Too many requests.'));
+      }
+      if (err.response.status === 401) {
+        return Promise.reject(new Error('Token error. Reauthorize Spotify and try again.'));
+      }
+    }
+    return Promise.reject(new Error('Error. Try again.'));
+  });
 
 /**
  * Get limit amount of Spotify artists from after
