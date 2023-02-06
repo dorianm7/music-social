c84a8783e849cf4812c1ac770cc2ad6f0821ea4a
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2023-02-06 15:55:21 -0800

    Create syncData function to sync albums,artists,playlists

--- src/backend/app/spotify.js
+++ src/backend/app/spotify.js
@@ -31,6 +31,93 @@ import {
 } from './spotify-helpers';
 
 /**
+ * Sync users Spotify data of given type to backend
+ * @param {string} type Spotify data type to sync to backend (albums,artists,playlists)
+ * @param {string} uid Id of user
+ * @param {string} accessToken Spotify access token
+ * @returns {Promise<void>[]} Array of successful operations
+ */
+const syncData = async (type, uid, accessToken) => {
+  let getDataItems;
+  if (type === 'albums' || type === 'playlists') {
+    getDataItems = getLimitOffsetItems;
+  } else if (type === 'artists') {
+    getDataItems = getLimitCursorItems;
+  } else {
+    return Promise.reject(new Error('Invalid type entered.'));
+  }
+
+  const [listItems, userRes] = await Promise.all([
+    getDataItems(type, accessToken),
+    getUser(uid, [`spotify_${type}`]),
+  ]);
+
+  const defaultDateIsoString = (new Date(0)).toISOString();
+  let userSpotifyDataObject;
+  let patchSpotifyDataDocument;
+  let firstTimeSync;
+  let createBackendDataDocument;
+  let listItemsForDb;
+  if (type === 'albums') {
+    userSpotifyDataObject = userRes.data.spotify_albums;
+    patchSpotifyDataDocument = patchUsersSpotifyAlbums;
+    firstTimeSync = userSpotifyDataObject.last_updated === defaultDateIsoString;
+    createBackendDataDocument = createUsersSpotifyAlbums;
+    listItemsForDb = formatAlbums(listItems);
+  } else if (type === 'artists') {
+    userSpotifyDataObject = userRes.data.spotify_artists;
+    patchSpotifyDataDocument = patchUsersSpotifyArtists;
+    firstTimeSync = userSpotifyDataObject.last_updated === defaultDateIsoString;
+    createBackendDataDocument = createUsersSpotifyArtists;
+    listItemsForDb = formatArtists(listItems);
+  } else {
+    userSpotifyDataObject = userRes.data.spotify_playlists;
+    patchSpotifyDataDocument = patchUsersSpotifyPlaylists;
+    firstTimeSync = userSpotifyDataObject.last_updated === defaultDateIsoString;
+    createBackendDataDocument = createUsersSpotifyPlaylists;
+    listItemsForDb = formatPlaylists(listItems);
+  }
+
+  let backendDataId;
+  const promises = [];
+  if (firstTimeSync) {
+    backendDataId = ObjectID();
+    await createBackendDataDocument(backendDataId);
+    promises.push(patchUser(
+      uid,
+      [{
+        op: 'replace',
+        path: `/spotify_${type}`,
+        value: {
+          items_id: backendDataId.toHexString(),
+          last_updated: (new Date()).toISOString(),
+        },
+      }],
+    ));
+  } else {
+    backendDataId = userSpotifyDataObject.items_id;
+    promises.push(patchUser(
+      uid,
+      [{
+        op: 'replace',
+        path: `/spotify_${type}/last_updated`,
+        value: (new Date()).toISOString(),
+      }],
+    ));
+  }
+  promises.push(patchSpotifyDataDocument(
+    backendDataId,
+    [{
+      op: 'replace',
+      path: '/items',
+      value: listItemsForDb,
+    }],
+  ));
+
+  return Promise.all(promises);
+};
+
+/**
  * Sync users spotify albums to backend
  * @param {string} uid Id of user
  * @param {string} accessToken Spotify access token
@@ -233,4 +320,5 @@ export {
   syncPlaylists,
   syncArtists,
   syncLibrary,
+  syncData,
 };
