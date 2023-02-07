a77602c61d213f94eb815b31af7c671d7e878fd3
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2023-02-06 17:58:09 -0800

    Create SpotifyClient and use updated spotify-endpoint constants

--- src/backend/spotify/spotify-endpoints.js
+++ src/backend/spotify/spotify-endpoints.js
@@ -4,45 +4,27 @@
  */
 
 const SPOTIFY_API_ENDPOINT = 'https://api.spotify.com/v1';
-const CURRENT_USER_PROFILE_ENDPOINT = `${SPOTIFY_API_ENDPOINT}/me`;
-const CURRENT_USER_PLAYLISTS_ENDPOINT = `${CURRENT_USER_PROFILE_ENDPOINT}/playlists`;
-const CURRENT_USER_ALBUMS_ENDPOINT = `${CURRENT_USER_PROFILE_ENDPOINT}/albums`;
-const CURRENT_USER_FOLLOWING_ENDPOINT = `${CURRENT_USER_PROFILE_ENDPOINT}/following`;
+const CURRENT_USER_PROFILE_ROUTE = '/me';
+const CURRENT_USER_PLAYLISTS_ROUTE = `${CURRENT_USER_PROFILE_ROUTE}/playlists`;
+const CURRENT_USER_ALBUMS_ROUTE = `${CURRENT_USER_PROFILE_ROUTE}/albums`;
+const CURRENT_USER_FOLLOWING_ROUTE = `${CURRENT_USER_PROFILE_ROUTE}/following`;
 
 /**
- * Get Spotify endpoint that will return user's specified albums
- * @param {int} limit Amount of albums to return
- * @param {int} offset Index of first item to return
- * @returns {string} Spotify endpoint
+ * Get query params for a limit cursor endpoint
+ * @param {int} limit Amount of items to return
+ * @param {string} after Id of last item Id returned from previous request
+ * @returns {string} Query string containing limit,after
  */
-const getUsersAlbumsHref = (limit = 50, offset = 0) => `${CURRENT_USER_ALBUMS_ENDPOINT}?limit=${limit}&offset=${offset}`;
-
-/**
- * Get Spotify endpoint that will return user's specified artists
- * @param {int} limit Amount of artists to return
- * @param {string} after Id of last artist Id returned from previous request
- * @returns {string} Spotify endpoint
- */
-const getUsersArtistsHref = (limit = 50, after = '') => {
+const getLimitCursorQueryParams = (limit = 50, after = '') => {
   const afterSearchParam = after ? `&after=${after}` : '';
-  return `${CURRENT_USER_FOLLOWING_ENDPOINT}?type=artist&limit=${limit}${afterSearchParam}`;
+  return `limit=${limit}${afterSearchParam}`;
 };
-
-/**
- * Get Spotify endpoint that will return user's specified playlists
- * @param {int} limit Amount of playlists to return
- * @param {int} offset Index of first item to return
- * @returns {string} Spotify endpoint
- */
-const getUsersPlaylistsHref = (limit = 50, offset = 0) => `${CURRENT_USER_PLAYLISTS_ENDPOINT}?limit=${limit}&offset=${offset}`;
 
 export {
   SPOTIFY_API_ENDPOINT,
-  CURRENT_USER_ALBUMS_ENDPOINT,
-  CURRENT_USER_FOLLOWING_ENDPOINT,
-  CURRENT_USER_PLAYLISTS_ENDPOINT,
-  CURRENT_USER_PROFILE_ENDPOINT,
-  getUsersAlbumsHref,
-  getUsersArtistsHref,
-  getUsersPlaylistsHref,
+  CURRENT_USER_PROFILE_ROUTE,
+  CURRENT_USER_PLAYLISTS_ROUTE,
+  CURRENT_USER_ALBUMS_ROUTE,
+  CURRENT_USER_FOLLOWING_ROUTE,
+  getLimitCursorQueryParams,
 };
