707fe234f4577ddf0fdb66d5b0cfd65c82d244c3
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2023-02-06 21:11:16 -0800

    Create and use Axios instance UsersSpotifyArtistsClient

--- src/backend/users_spotify_artists/users_spotify_artists-helpers.js
+++ src/backend/users_spotify_artists/users_spotify_artists-helpers.js
@@ -5,14 +5,4 @@
 
 const USERS_SPOTIFY_ARTISTS_BACKEND_ENDPOINT = `${process.env.REACT_APP_BACKEND_HTTP_SERVER}/users_spotify_artists`;
 
-/**
- * Returns the backend endpoint of document with given uid
- * @param {string} uid Id of the document
- * @returns {string} Endpoint of the document
- */
-const usersSpotifyArtistsBaseEndpoint = (uid) => `${USERS_SPOTIFY_ARTISTS_BACKEND_ENDPOINT}/${uid}`;
-
-export {
-  USERS_SPOTIFY_ARTISTS_BACKEND_ENDPOINT,
-  usersSpotifyArtistsBaseEndpoint,
-};
+export default USERS_SPOTIFY_ARTISTS_BACKEND_ENDPOINT;
