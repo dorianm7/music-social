a5625d5e8c5a5739e696e734b4b5fae5a06b5380
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2023-02-07 11:10:38 -0800

    Create createUser function to create user in backend and firebase

--- src/backend/app/user.js
+++ src/backend/app/user.js
@@ -3,10 +3,11 @@
  * @module user
  */
 
-import { deleteUserAccount } from '../../firebase/auth-firebase';
+import { deleteUserAccount, userSignUp } from '../../firebase/auth-firebase';
 import { removeAccessToken } from '../spotify/spotify-auth';
 import {
   deleteUser as deleteUserFromDb,
+  createUser as createUserInDb,
 } from '../users/users';
 
 /**
@@ -22,4 +23,18 @@ const deleteUser = async (uid) => {
   ]);
 };
 
-export default deleteUser;
+/**
+ * Create user in backend and firebase
+ * @param {string} email Email of user
+ * @param {string} password Password of user
+ * @returns {Promise<void>} Promise of successful operations
+ */
+const createUser = async (email, password) => {
+  const user = await userSignUp(email, password);
+  await createUserInDb(user.uid, email);
+};
+
+export {
+  deleteUser,
+  createUser,
+};
