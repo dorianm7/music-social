b5381ba278565f79790432e8d58ffb0ffb069161
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2023-02-08 00:25:09 -0800

    Create signInGoogleUser

--- src/backend/app/user.js
+++ src/backend/app/user.js
@@ -3,11 +3,17 @@
  * @module user
  */
 
-import { deleteUserAccount, userSignOut, userSignUp } from '../../firebase/auth-firebase';
+import {
+  deleteUserAccount,
+  googleSignIn,
+  userSignOut,
+  userSignUp,
+} from '../../firebase/auth-firebase';
 import { removeAccessToken } from '../spotify/spotify-auth';
 import {
   deleteUser as deleteUserFromDb,
   createUser as createUserInDb,
+  getUser as getUserFromDb,
 } from '../users/users';
 
 /**
@@ -42,8 +48,27 @@ const signOutUser = () => {
   userSignOut();
 };
 
+/**
+ * Sign in user using Google, create user in backend if needed
+ */
+const signInGoogleUser = async () => {
+  const user = await googleSignIn();
+  const userFromDb = await getUserFromDb(user.uid, ['_id'])
+    .catch((err) => {
+      if (err.message !== 'Error. User not found.') {
+        return Promise.reject(err);
+      }
+      return null; // Quiet linter
+    });
+
+  if (!userFromDb) {
+    await createUserInDb(user.uid, user.providerData[0].email);
+  }
+};
+
 export {
   deleteUser,
   createUser,
   signOutUser,
+  signInGoogleUser,
 };
