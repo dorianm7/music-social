71750c801ba1f02f64d40ec3b1e4e933cd624380
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2023-02-09 23:11:31 -0800

    Update stories

--- src/containers/pages/MainPage/MainPage.stories.jsx
+++ src/containers/pages/MainPage/MainPage.stories.jsx
@@ -1,15 +1,39 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 
-import MainPage from './MainPage';
-import MainNav from '../../../components/MainNav/MainNav';
+import AfterReportModalContents from '../../../components/modal-contents/AfterReportModalContents/AfterReportModalContents';
+import BasicButton from '../../../components/basic/BasicButton/BasicButton';
+import BasicPlaylist from '../../../components/BasicPlaylist/BasicPlaylist';
+import CollaborativePlaylistPageContents from '../../page-contents/CollaborativePlaylistPageContents/CollaborativePlaylistPageContents';
+import Comparison from '../../../components/Comparison/Comparison';
 import Footer from '../../../components/Footer/Footer';
+import MainNav from '../../../components/MainNav/MainNav';
+import MainPage from './MainPage';
+import MusicLibrary from '../../../components/MusicLibrary/MusicLibrary';
+import PlaylistHeader from '../../../components/PlaylistHeader/PlaylistHeader';
+import ReportProfileForm from '../../../components/forms/ReportProfileForm/ReportProfileForm';
+import Tabs from '../../../components/subcomponents/Tabs/Tabs';
+import UserProfileHeader from '../../../components/UserProfileHeader/UserProfileHeader';
 
 export default {
   title: 'Containers/Pages/MainPage',
   component: MainPage,
+  decorators: [
+    (Story) => <MemoryRouter><Story /></MemoryRouter>,
+  ],
   subcomponents: {
     MainNav,
+    Tabs,
+    MusicLibrary,
+    CollaborativePlaylistPageContents,
+    UserProfileHeader,
+    BasicPlaylist,
+    AfterReportModalContents,
+    BasicButton,
+    PlaylistHeader,
+    ReportProfileForm,
+    Comparison,
     Footer,
   },
   parameters: {
