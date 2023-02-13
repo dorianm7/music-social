67ec40e5931fdb83e5ffa96222dd1e6892073874
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2023-02-13 00:18:18 -0800

    Retrieve and render library totals

--- src/containers/page-contents/HomePageContents/HomePageContents.jsx
+++ src/containers/page-contents/HomePageContents/HomePageContents.jsx
@@ -16,7 +16,8 @@ import LibraryInfo from '../../../components/LibraryInfo/LibraryInfo';
 
 function HomePageContents(props) {
   const { setInAppPageTitle } = props;
-  const [syncData, setSyncData] = useState((new Date(0)));
+  const [syncDate, setSyncDate] = useState((new Date(0)));
+  const [libraryTotals, setLibraryTotals] = useState(null);
   const [loading, setLoading] = useState(true);
   const user = useUserContext();
   const authorized = isAuthorized();
@@ -24,13 +25,21 @@ function HomePageContents(props) {
   useEffect(async () => {
     setInAppPageTitle('Home');
     document.title = 'Music Social | Home';
-    const userRes = await getUser(user.uid, ['spotify_albums']);
-    const spotifyAlbums = userRes.data.spotify_albums;
-    setSyncData(new Date(spotifyAlbums.last_updated));
+    const userRes = await getUser(user.uid, [
+      'spotify_albums',
+      'spotify_artists',
+      'spotify_playlists',
+    ]);
+    setSyncDate(new Date(userRes.data.spotify_albums.last_updated));
+    setLibraryTotals({
+      albums: userRes.data.spotify_albums.total,
+      artists: userRes.data.spotify_artists.total,
+      playlists: userRes.data.spotify_playlists.total,
+    });
     setLoading(false);
   }, []);
 
-  const hasSynced = syncData.getTime() > (new Date(0)).getTime();
+  const hasSynced = syncDate.getTime() > (new Date(0)).getTime();
 
   return loading
     ? <></>
@@ -51,11 +60,15 @@ function HomePageContents(props) {
         {authorized && hasSynced && (
           <div className="authorized synced center-column">
             <span>Library info</span>
-            <LibraryInfo />
+            <LibraryInfo
+              albumsTotal={libraryTotals.albums}
+              artistsTotal={libraryTotals.artists}
+              playlistsTotal={libraryTotals.playlists}
+            />
             <BasicButton>Sync music library</BasicButton>
             <span>
               Last sync:
-              {syncData.toLocaleString()}
+              {syncDate.toLocaleString()}
             </span>
           </div>
         )}
